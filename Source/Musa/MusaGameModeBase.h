17a84b691e1f4e1b5bd6874b40545ff6d899597a
elisfromkirov <mozgg2002@gmail.com>
2023-04-30 00:36:34 +0300

    create unreal engine project

---
+++ Source/Musa/MusaGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MusaGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MUSA_API AMusaGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
